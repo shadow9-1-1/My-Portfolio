9d0b19d9617c5561702c61e729a9ab709bb80e8d
Ahmed Wael <133889737+shadow9-1-1@users.noreply.github.com>
2026-02-10 17:48:14 +0200

    fix npm run build plugins report

--- TheCode/vite.config.js
+++ TheCode/vite.config.js
@@ -5,4 +5,23 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom') || id.includes('react-router')) {
+              return 'vendor-react'
+            }
+            if (id.includes('gsap')) {
+              return 'vendor-gsap'
+            }
+            if (id.includes('motion')) {
+              return 'vendor-motion'
+            }
+          }
+        },
+      },
+    },
+  },
 })
